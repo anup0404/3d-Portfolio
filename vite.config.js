964549b5b4526178b7602f672905c58c77c026cf
Anup Kumar <anupkumarggupta@gmail.com>
2023-12-10 22:45:04 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
-  build: {
-    rollupOptions: {
-      external: ["@react-three/drei"],
-    },
-  },
 });
